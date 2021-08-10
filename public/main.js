620af8127af32fe072d11900ef91d7868a87a32b
Rman22 <rmanmekini@gmail.com>
2021-08-10 22:27:54 +0800

    update81021

--- public/main.js
+++ public/main.js
@@ -14,3 +14,12 @@ console.log('hello');
         }
       });
     });
+
+
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "280px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
     extend: {
       colors:{
         'customGreen':'#39b54a;',
-        'customBlack':'#121212;'
-      }
+        'customBlack':'#121212;',
+        'customBlack2':'rgba(0, 0, 0, 0.3)',
+         
+      },
+      visibility: ['hover', 'focus'],
     },
 
   },
