d82b2c0b9fea785f1608ff9eb905a54bf9c039ce
Rman22 <rmanmekini@gmail.com>
2021-08-12 18:28:15 +0800

    update

--- public/main.js
+++ public/main.js
@@ -1,22 +1,4 @@
-
-
-console.log('hello');
-  (function() {
-      // Smooth Scrolling
-      $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1500);
-            return false;
-          }
-        }
-      });
-    });
-
+//////menu bar
 
     function openNav() {
       document.getElementById("mySidenav").style.width = "280px";
@@ -26,38 +8,34 @@ console.log('hello');
       document.getElementById("mySidenav").style.width = "0";
     }
 
-    ///count up numbers
-    /** 
+///count up numbers
+    
+    var $counter = $('.counter');
+    $counter.waypoint(function (){
+      console.log('waypointhere');
+ 
+    })
+     
+    console.log('waypoint here');
     $(document).ready(function(){
-   const counter = document.querySelectorAll('.num');
-
-   counter.forEach((num) =>{
-     num.innerText = '0';
-
-     const updateCounter = () => {
-       const target = +num.getAttribute
-       ('data-target');
-
-       const c = +num.innerText
-       const increment = target / 50;
-
-       if (c < target) {
-         num.innerText = `${Math.ceil(c + increment)}`;
-         setTimeout(updateCounter, 10);
-       }
-
-     };
-     updateCounter();
-   })
-  }
-  )
-*/
-
-//counterup
+        $('.counter').counterUp({
+            delay: 2,
+            time: 1500
+        });
+     
+      });
+/**
+var waypoint = new Waypoint({
+  element: document.getElementById('counter'),
+  handler: function(direction) {
+    
 $(document).ready(function(){
   $('.counter').counterUp({
       delay: 10,
-      time: 1200
+      time: 2000
   });
 
 });
+  }
+})
+ */
